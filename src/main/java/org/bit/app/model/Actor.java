44359141804158e0686f5fcf2d49c64b85560b2b
Rifky Hamdhani <rifky.hamdani@ist.id>
2025-08-25 15:56:11 +0700

    Initial commit

---
+++ src/main/java/org/bit/app/model/Actor.java
@@ -0,0 +1,58 @@
+package org.bit.app.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Entity(name = "Actor")
+@Table(name = "actor", schema = "sakila")
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "actor_id", nullable = false)
+    private Short id;
+
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+}
